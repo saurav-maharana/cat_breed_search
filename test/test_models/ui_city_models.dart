e06189d9054cf591ab81efa0cc6eb8bdf7280bc6
saurav-maharana <104126081+saurav-maharana@users.noreply.github.com>
2022-04-25 12:20:22 +0530

    Initial commit

---
+++ test/test_models/ui_city_models.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_template/presentation/entity/weather/ui_city.dart';
+
+final uiCity = UICity(
+  cityId: 1,
+  title: "title",
+  locationType: "locationType",
+  location: "location",
+  isFavourite: false,
+);
+
+final searchList = [
+  uiCity,
+  uiCity.copyWith(cityId: 2, title: "title 2", isFavourite: true),
+];
