6f8d5c1761dae07824244532aee14b07fd461b8b
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-04-26 10:57:25 +0530

    removed comments

--- lib/repository/cat/domain_cat_mapper.dart
+++ lib/repository/cat/domain_cat_mapper.dart
@@ -2,12 +2,7 @@ import 'package:flutter_template/domain/entity/cat/cat.dart';
 import 'package:flutter_template/foundation/mapper/mapper.dart';
 import 'package:flutter_template/services/entity/cat/remote/remote_cat.dart';
 
-abstract class DomainCatMapper extends Mapper<RemoteCat, Cat> {
-  // RemoteCat mapRemoteCat(RemoteCat from);
-
-  // List<RemoteCat> mapRemoteCatList(List<RemoteCat> from) =>
-  //     from.map((e) => mapRemoteCat(e)).toList();
-}
+abstract class DomainCatMapper extends Mapper<RemoteCat, Cat> {}
 
 class DomainCatMapperImpl extends DomainCatMapper {
   @override
