7d79382416f21509434f86689e13dbd38a7c259d
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-04-26 14:55:46 +0530

    fixed class names

--- lib/repository/cat/cat_repository_impl.dart
+++ lib/repository/cat/cat_repository_impl.dart
@@ -1,21 +1,20 @@
+import 'package:flutter_template/repository/cat/cat_repository.dart';
 import 'package:flutter_template/repository/cat/domain_cat_mapper.dart';
-import 'package:flutter_template/services/cat/remote/cat_remote_service.dart';
 import 'package:flutter_template/services/entity/cat/remote/remote_cat.dart';
 
-class CatRepositoryImpl implements CatRemoteService {
-  final CatRemoteService catRemoteService;
+class CatRepositoryImpl implements CatRepository {
+  final CatRepository catRepository;
   final DomainCatMapper domainCatMapper;
 
   CatRepositoryImpl({
-    required this.catRemoteService,
+    required this.catRepository,
     required this.domainCatMapper,
   });
 
   @override
-  Future<List<RemoteCat>> searchCat({required String searchTerm}) async {
-    final results = await catRemoteService.searchCat(
-      searchTerm: searchTerm,
-    );
-    return results;
+  Future<List<RemoteCat>> searchCat(String searchTerm) async {
+    final result = await catRepository.searchCat(searchTerm);
+
+    return result;
   }
 }
