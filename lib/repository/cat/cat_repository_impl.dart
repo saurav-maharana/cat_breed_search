873a11609a956f87b36ed20a72c70d6ebdbad045
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-04-27 16:25:46 +0530

    fixed cyclic dependency

--- lib/repository/cat/cat_repository_impl.dart
+++ lib/repository/cat/cat_repository_impl.dart
@@ -1,14 +1,14 @@
 import 'package:flutter_template/repository/cat/cat_repository.dart';
-import 'package:flutter_template/repository/cat/domain_cat_mapper.dart';
+import 'package:flutter_template/services/cat/remote/cat_remote_service.dart';
 import 'package:flutter_template/services/entity/cat/remote/remote_cat.dart';
 
 class CatRepositoryImpl implements CatRepository {
   final CatRepository catRepository;
-  final DomainCatMapper domainCatMapper;
+  final CatRemoteService catRemoteService;
 
   CatRepositoryImpl({
     required this.catRepository,
-    required this.domainCatMapper,
+    required this.catRemoteService,
   });
 
   @override
