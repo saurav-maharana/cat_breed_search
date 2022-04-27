e535df433ce35bf6a2ab2b0515ee730695b7b572
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-04-27 16:52:13 +0530

    added second text case to check for List of cats

--- test/repository/cat/domain_cat_mapper_impl_test.dart
+++ test/repository/cat/domain_cat_mapper_impl_test.dart
@@ -18,13 +18,31 @@ void main() {
 
   test('Given remote cat, when map remote cat is called, then CAT is returned',
       () async {
+    // Given
     final remoteCatData = remoteCat1;
 
+    // When
     final result = domainCatMapper.map(remoteCatData);
 
+    // Then
     expect(result, isA<Cat>());
     expect(result.id, remoteCatData.id);
     expect(result.name, remoteCatData.name);
     expect(result.origin, remoteCatData.origin);
+  });
+
+  test('Given List of Cat, and map is called; then Cat list is returned',
+      () async {
+    // Given
+
+    final remoteCatList = allCatList;
+
+    // Where
+
+    final result = domainCatMapper.mapList(remoteCatList);
+
+    // Then
+
+    expect(result, isA<List<Cat>>());
   });
 }
