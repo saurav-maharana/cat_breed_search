9011378188373f4c4e14d35ee8cebadda4589693
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-04-28 13:31:30 +0530

    fixed remote cat to Cat. Removed unusued imports

--- lib/domain/cat/search_cat_use_case.dart
+++ lib/domain/cat/search_cat_use_case.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_template/domain/base/usecase/base_async_use_case.dart';
-import 'package:flutter_template/services/entity/cat/remote/remote_cat.dart';
+import 'package:flutter_template/domain/entity/cat/cat.dart';
 
-abstract class SearchCatUseCase
-    extends BaseAsyncUseCase<String, List<RemoteCat>> {}
+abstract class SearchCatUseCase extends BaseAsyncUseCase<String, List<Cat>> {}
