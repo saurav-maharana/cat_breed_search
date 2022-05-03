503fb21e698f3773d809d93ec2e822328c9294cb
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-05-03 17:43:56 +0530

    cat search api view model impl

---
+++ lib/presentation/destinations/cat/search/search_screen_view_model_impl.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_template/interactor/cat/search/search_cat_interactor.dart';
+import 'package:flutter_template/navigation/weather/search/search_navigator.dart';
+import 'package:flutter_template/presentation/destinations/cat/search/search_screen_view_model.dart';
+import 'package:flutter_template/presentation/destinations/weather/search/search_screen_intent.dart';
+import 'package:flutter_template/presentation/destinations/weather/search/search_screen_state.dart';
+import '../../../entity/base/ui_toolbar.dart';
+
+class CatSearchViewModelImpl extends CatSearchViewModel {
+  final SearchCatInteractor searchCatInteractor;
+  final SearchNavigator searchNavigator;
+  final String _searchTerm = "";
+
+  CatSearchViewModelImpl({
+    required this.searchCatInteractor,
+    required this.searchNavigator,
+  }) : super(_initialState);
+
+  static SearchScreenState get _initialState => SearchScreenState(
+        toolbar: UIToolbar(
+          title: "Search Page Title",
+          hasBackButton: true,
+        ),
+        showLoading: false,
+        searchList: List.empty(),
+      );
+
+  @override
+  onInit() {
+    setState((state) => state.copyWith(showLoading: true));
+    searchCatInteractor.search('Sib').then((_) => {
+          state.copyWith(
+            showLoading: false,
+            searchList: [],
+          ),
+        });
+  }
+
+  @override
+  void onIntent(SearchScreenIntent intent) {}
+
+  @override
+  String get searchTerm => _searchTerm;
+}
