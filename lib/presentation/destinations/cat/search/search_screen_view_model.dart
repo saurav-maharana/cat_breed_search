9751afec8144eb5fda9e6d0ec33899a430502aad
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-05-04 11:11:14 +0530

    removed intent: was blank

--- lib/presentation/destinations/cat/search/search_screen_view_model.dart
+++ lib/presentation/destinations/cat/search/search_screen_view_model.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_template/presentation/base/intent/intent_handler.dart';
-import 'package:flutter_template/presentation/base/view_model_provider/base_view_model.dart';
-import 'package:flutter_template/presentation/destinations/weather/search/search_screen_intent.dart';
 import 'package:flutter_template/presentation/destinations/weather/search/search_screen_state.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../../base/view_model_provider/base_view_model.dart';
 import '../../../entity/screen/screen.dart';
 
 final searchViewModelProvider =
@@ -12,8 +10,7 @@ final searchViewModelProvider =
         (ref) => GetIt.I.get());
 
 abstract class CatSearchViewModel
-    extends BaseViewModel<SearchScreen, SearchScreenState>
-    implements IntentHandler<SearchScreenIntent> {
+    extends BaseViewModel<SearchScreen, SearchScreenState> {
   CatSearchViewModel(SearchScreenState state) : super(state);
 
   String get searchTerm;
