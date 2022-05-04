a695609f69aabbbeec58cce3ea6b29b18e6e2c21
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-05-04 14:41:14 +0530

    fixed imports - now using package imports instead of .imports

--- lib/presentation/destinations/cat/search/search_screen_view_model.dart
+++ lib/presentation/destinations/cat/search/search_screen_view_model.dart
@@ -1,9 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_template/presentation/base/view_model_provider/base_view_model.dart';
 import 'package:flutter_template/presentation/destinations/cat/search/search_screen_state.dart';
+import 'package:flutter_template/presentation/entity/screen/screen.dart';
 import 'package:get_it/get_it.dart';
-
-import '../../../base/view_model_provider/base_view_model.dart'; // convert to package import
-import '../../../entity/screen/screen.dart'; // convert to package import
 
 final catSearchViewModelProvider =
     StateNotifierProvider.autoDispose<CatSearchViewModel, CatSearchScreenState>(
