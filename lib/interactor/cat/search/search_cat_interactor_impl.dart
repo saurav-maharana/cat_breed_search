bf13655eae1ddfd28a73817853dcec3cb79b2af4
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-05-04 11:26:26 +0530

    fixed typecast: adding to list as Cat

--- lib/interactor/cat/search/search_cat_interactor_impl.dart
+++ lib/interactor/cat/search/search_cat_interactor_impl.dart
@@ -1,18 +1,19 @@
 import 'dart:async';
 import 'package:flutter_template/domain/cat/search_cat_use_case.dart';
+import 'package:flutter_template/domain/entity/cat/cat.dart';
 import 'package:flutter_template/foundation/logger/logger.dart';
 import 'package:flutter_template/interactor/cat/search/search_cat_interactor.dart';
 
 class SearchCatInteractorImpl extends SearchCatInteractor {
   late final SearchCatUseCase searchCatUseCase;
-  List searchCatResult = [];
+  List<Cat> searchCatResult = [];
 
   @override
   Future<void> search(String searchTerm) async {
     final searchResults = await searchCatUseCase(param: searchTerm);
 
     searchResults.when(
-      success: ((data) => searchCatResult.add(data)),
+      success: ((data) => searchCatResult.add(data as Cat)),
       error: ((e) => log.e(
           "Search Cat Implemetation: Search $searchTerm returned error. $e")),
     );
