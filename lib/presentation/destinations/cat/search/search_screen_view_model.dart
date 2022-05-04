425b8dd2149f0c2302f04d5367a8b0b211d584ce
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-05-04 14:39:16 +0530

    fixed class names

--- lib/presentation/destinations/cat/search/search_screen_view_model.dart
+++ lib/presentation/destinations/cat/search/search_screen_view_model.dart
@@ -1,17 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_template/presentation/destinations/weather/search/search_screen_state.dart';
+import 'package:flutter_template/presentation/destinations/cat/search/search_screen_state.dart';
 import 'package:get_it/get_it.dart';
 
-import '../../../base/view_model_provider/base_view_model.dart';
-import '../../../entity/screen/screen.dart';
+import '../../../base/view_model_provider/base_view_model.dart'; // convert to package import
+import '../../../entity/screen/screen.dart'; // convert to package import
 
-final searchViewModelProvider =
-    StateNotifierProvider.autoDispose<SearchScreen, SearchScreenState>(
+final catSearchViewModelProvider =
+    StateNotifierProvider.autoDispose<CatSearchViewModel, CatSearchScreenState>(
         (ref) => GetIt.I.get());
 
 abstract class CatSearchViewModel
-    extends BaseViewModel<SearchScreen, SearchScreenState> {
-  CatSearchViewModel(SearchScreenState state) : super(state);
+    extends BaseViewModel<CatSearchScreen, CatSearchScreenState> {
+  CatSearchViewModel(CatSearchScreenState state) : super(state);
 
   String get searchTerm;
 }
