5887d927bb9574810807d4c70f896a05b5bb5401
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-05-04 11:26:58 +0530

    added cast: now adding items to list as UIListItem

--- lib/presentation/destinations/cat/search/search_screen_view_model_impl.dart
+++ lib/presentation/destinations/cat/search/search_screen_view_model_impl.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_template/interactor/cat/search/search_cat_interactor.dart';
+import 'package:flutter_template/interactor/cat/search/search_cat_interactor_impl.dart';
 import 'package:flutter_template/navigation/weather/search/search_navigator.dart';
 import 'package:flutter_template/presentation/destinations/cat/search/search_screen_view_model.dart';
 import 'package:flutter_template/presentation/destinations/weather/search/search_screen_state.dart';
+import 'package:flutter_template/presentation/entity/base/ui_list_item.dart';
 import '../../../entity/base/ui_toolbar.dart';
 
 class CatSearchViewModelImpl extends CatSearchViewModel {
@@ -29,7 +31,8 @@ class CatSearchViewModelImpl extends CatSearchViewModel {
     searchCatInteractor.search('Sib').then((_) => {
           state.copyWith(
             showLoading: false,
-            searchList: [], // pass searchCatResults list here from search_cat_interactor_impl.dart file
+            searchList:
+                SearchCatInteractorImpl().searchCatResult as List<UIListItem>,
           ),
         });
   }
