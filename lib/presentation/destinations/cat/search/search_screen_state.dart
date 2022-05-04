92ae2f21a701d51e58e599ac716c219182afacde
Saurav Maharana <saurav@Sauravs-MacBook-Pro.local>
2022-05-04 14:37:59 +0530

    fixed implements class

--- lib/presentation/destinations/cat/search/search_screen_state.dart
+++ lib/presentation/destinations/cat/search/search_screen_state.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_template/presentation/entity/base/ui_toolbar.dart';
-import 'package:flutter_template/presentation/entity/screen/cat/screen_state.dart';
+import 'package:flutter_template/presentation/entity/screen/screen_state.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'search_screen_state.freezed.dart';
 
 @freezed
-class CatSearchScreenState
-    with _$CatSearchScreenState
-    implements CatScreenState {
+class CatSearchScreenState with _$CatSearchScreenState implements ScreenState {
   factory CatSearchScreenState({
     required UIToolbar toolbar,
     required bool showLoading,
