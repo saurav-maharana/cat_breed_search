eb93027ed84ac403507e02347ba1be5ce90f55b4
Saurav Maharana <saurav@wednesday.is>
2022-05-08 15:11:01 +0530

    added listeners, intentCallBack, & passed searchTerm

--- lib/presentation/destinations/cat/search/widgets/search_page_results/search_page_results.dart
+++ lib/presentation/destinations/cat/search/widgets/search_page_results/search_page_results.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_template/presentation/destinations/cat/search/search_screen_view_model.dart';
 import 'package:flutter_template/presentation/destinations/cat/search/widgets/search_page_results/search_page_results_content.dart';
 
 class CatSearchPageResults extends ConsumerWidget {
@@ -9,8 +10,11 @@ class CatSearchPageResults extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const CatSearchPageResultsContent(
-      searchTerm: 'Sib',
+    final modelSearchTerm = ref.watch(catSearchViewModelProvider.notifier);
+
+    return CatSearchPageResultsContent(
+      searchTerm: modelSearchTerm.searchTerm,
+      intentHandlerCallback: modelSearchTerm.onIntent,
     );
   }
 }
